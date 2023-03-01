f64d0ee0d8564e9406b0af50484d42efe494cb76
Florian Lepage <florian.kyn@gmail.com>
2023-03-01 02:24:15 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,53 @@
+//index.js// -- Created By FLorian Lepage
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+const client = new Client(
+    {
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.GuildMembers,
+            GatewayIntentBits.DirectMessageReactions,
+            GatewayIntentBits.DirectMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildPresences,
+            GatewayIntentBits.GuildMessageReactions,
+            GatewayIntentBits.GuildIntegrations
+        ], partials: [
+            Partials.Channel,
+            Partials.Reaction,
+            Partials.Message,
+            Partials.GuildMember
+        ]
+    }
+);
+
+const config = require("./config/config.json");
+
+//Libs imports
+const { CommandsLoader } = require("./components/CommandsLoader.js");
+const { SQLTablesManager } = require("./components/SQLTablesManager.js");
+const { Database } = require("./database/Database.js");
+
+//Modules Imports
+const { ToyCreationManager } = require("./components/ToyCreationManager.js");
+
+client.on('ready', async() => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    await new Database(config).checkConnectionState();
+
+    await new SQLTablesManager(config, [
+        ToyCreationManager
+    ]).loadTables();
+
+    await new CommandsLoader(client, [
+        ToyCreationManager
+    ], config).loadCommands();
+});
+
+client.on("interactionCreate", async (interaction) => {
+    await new ToyCreationManager(interaction, client, config).on();
+});
+
+client.login(config.discord.token).then().catch();
